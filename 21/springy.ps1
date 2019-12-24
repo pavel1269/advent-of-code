a3af0a442be5e59c03b0b1ad2ccb9d506c3c7605
pavel1269 <pavelgrim@gmail.com>
2019-12-24 11:23:36 +0100

    Puzzle 21, part 2

--- 21/springy.ps1
+++ 21/springy.ps1
@@ -19,8 +19,14 @@ function Walk-Springy {
     
     $res = IntComp -OpCodes ($OpCode.Clone()) -InputParams $codedCommands
 
+    $damage = $res.Outputs[$res.Outputs.Count - 1]
+    if ($damage -gt 255) {
+        $res.Outputs[$res.Outputs.Count - 1] = [int][char]' '
+    }
+
     $printableOut = [string][char[]][int[]]$res.Outputs
     write-host $printableOut
+    write-host "Damage: $damage"
 }
 
 function Get-Part1Result {
@@ -33,8 +39,8 @@ function Get-Part1Result {
         "OR A T"
         "AND B T"
         "AND C T" # T = ABC
-        "NOT T J" # T = !(ABC)
-        "AND D J" # T = !(ABC)D
+        "NOT T J" # J = !(ABC)
+        "AND D J" # J = !(ABC)D
         "WALK"
     )
 
@@ -42,4 +48,36 @@ function Get-Part1Result {
 
     # Cannot convert value "19350375" to type "System.Char". Error: "Value was either too large or too small for a character." - ugly but works
     # 19350375 - correct
+}
+
+function Get-Part2Result {
+    [CmdletBinding()]
+    param()
+
+    $OpCode = Load-IntCompProgram "./program.txt"
+
+    $commands = @(
+        "OR E T"
+        "AND F T" # T = EF
+
+        "OR E J"
+        "AND I J"
+        "OR H J"
+        "OR T J" # J = H or EI or EF
+
+        "NOT A T"
+        "NOT T T" # T = A
+        "AND B T"
+        "AND C T" # T = ABC
+        "NOT T T" # T = !(ABC)
+        "AND D T" # T = !(ABC)D
+
+        "AND T J"
+
+        "RUN"
+    )
+
+    Walk-Springy $OpCode $commands
+
+    # 1143990055 - correct
 }
