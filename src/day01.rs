b546e92ce53705cdb8c72bdb4960abb38fc0d471
pave1269 <pavelgrim@gmail.com>
2020-12-03 18:54:41 +0100

    Day 1 improvements based on comments

--- src/day01.rs
+++ src/day01.rs
@@ -1,19 +1,19 @@
 
 pub fn get_solution_day01_part1() -> i64 {
-    let entries = get_part1_input();
-    let result = get_two_entries_which_sum(2020, entries);
+    let entries = get_input();
+    let result = get_two_entries_which_sum(2020, &entries).unwrap();
 
     return (result.0 * result.1) as i64;
 }
 
 pub fn get_solution_day01_part2() -> i64 {
-    let entries = get_part1_input();
-    let result = get_three_entries_which_sum(2020, entries);
+    let entries = get_input();
+    let result = get_three_entries_which_sum(2020, &entries).unwrap();
 
     return (result.0 * result.1 * result.2) as i64;
 }
 
-fn get_two_entries_which_sum(sum_value: i32, entries: Vec<i32>) -> (i32, i32) {
+fn get_two_entries_which_sum(sum_value: i32, entries: &[i32]) -> Result<(i32, i32), Box<String>> {
     for (index1, entry1) in entries.iter().enumerate() {
         for (index2, entry2) in entries.iter().enumerate() {
             if index1 == index2 {
@@ -21,15 +21,16 @@ fn get_two_entries_which_sum(sum_value: i32, entries: Vec<i32>) -> (i32, i32) {
             }
 
             if entry1 + entry2 == sum_value {
-                return (*entry1, *entry2);
+                return Ok((*entry1, *entry2));
             }
         }
     }
 
-    panic!("Could not find any two values wich sum to '{}'", sum_value);
+    let error = Box::new(format!("Could not find any two values wich sum to '{}'", sum_value));
+    return Err(error);
 }
 
-fn get_three_entries_which_sum(sum_value: i32, entries: Vec<i32>) -> (i32, i32, i32) {
+fn get_three_entries_which_sum(sum_value: i32, entries: &[i32]) -> Result<(i32, i32, i32), Box<String>> {
     for (index1, entry1) in entries.iter().enumerate() {
         for (index2, entry2) in entries.iter().enumerate() {
             if index1 == index2 {
@@ -45,32 +46,28 @@ fn get_three_entries_which_sum(sum_value: i32, entries: Vec<i32>) -> (i32, i32, 
                 }
 
                 if entry1 + entry2 + entry3 == sum_value {
-                    return (*entry1, *entry2, *entry3);
+                    return Ok((*entry1, *entry2, *entry3));
                 }
             }
         }
     }
 
-    panic!("Could not find any two values wich sum to '{}'", sum_value);
+    let error = Box::new(format!("Could not find any three values wich sum to '{}'", sum_value));
+    return Err(error);
 }
 
-fn get_part1_input() -> Vec<i32> {
+fn get_input() -> Vec<i32> {
     let file_contents = include_str!("./inputs/d01p1.txt").split_whitespace();
-
-    let entries = file_contents.map(|entry| match entry.trim().parse::<i32>() {
-        Ok(num) => num,
-        Err(err) => {
-            let error_message = err.to_string() + "\n";
-            panic!("Failed to convert number to integer, detail: {}", error_message);
-        },
-    }).collect();
+    let entries = file_contents.map(|entry| entry.trim().parse().expect("Failed to convert number to integer")).collect();
 
     return entries;
 }
 
-#[allow(dead_code)] // for unit test
-fn get_example_input() -> Vec<i32> {
-    let input = vec![
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EXAMPLE_INPUT: &[i32] = &[
         1721,
         979,
         366,
@@ -78,18 +75,10 @@ fn get_example_input() -> Vec<i32> {
         675,
         1456,
     ];
-    
-    return input;
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
 
     #[test]
     fn example_part1() {
-        let example_input = get_example_input();
-        let entries = get_two_entries_which_sum(2020, example_input);
+        let entries = get_two_entries_which_sum(2020, &EXAMPLE_INPUT).unwrap();
 
         assert_eq!((1721, 299), entries);
     }
@@ -104,8 +93,7 @@ mod tests {
 
     #[test]
     fn example_part2() {
-        let example_input = get_example_input();
-        let entries = get_three_entries_which_sum(2020, example_input);
+        let entries = get_three_entries_which_sum(2020, &EXAMPLE_INPUT).unwrap();
 
         assert_eq!((979, 366, 675), entries);
     }
