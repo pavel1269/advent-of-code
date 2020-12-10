6e5d49b924f2ce96b5c2655515e8166594c930f7
pave1269 <pavelgrim@gmail.com>
2020-12-10 20:42:56 +0100

    Day 8, part 2

--- src/day08.rs
+++ src/day08.rs
@@ -1,43 +1,20 @@
 
 pub fn get_part1_result() -> i64 {
     let input = get_challenge_input();
-    let result = detect_loop(input);
+    let result = detect_loop_input(input);
 
     return result;
 }
 
-fn detect_loop(input: &str) -> i64 {
-    let instructions = parse_program(input);
+pub fn get_part2_result() -> i64 {
+    let input = get_challenge_input();
+    let result = fix_and_detect_loop(input);
 
-    let mut accumulator: i64 = 0;
-    let mut program_counter: i64 = 0;
-
-    let mut instructions: Vec<(&Instruction, bool)> = instructions.iter().map(|ins| (ins, false)).collect();
-    loop {
-        let mut instruction = &mut instructions[program_counter as usize];
-
-        if instruction.1 {
-            break;
-        }
-        instruction.1 = true;
-
-        match instruction.0.operation {
-            Operation::Nop => {
-                program_counter += 1;
-            },
-            Operation::Acc => {
-                accumulator += instruction.0.argument as i64;
-                program_counter += 1;
-            },
-            Operation::Jmp => {
-                program_counter += instruction.0.argument as i64;
-            }
-        }
-    }
-
-    return accumulator;
+    return result;
 }
 
+#[derive(Clone)]
+#[derive(Debug)]
 enum Operation {
     Nop = 0,
 
@@ -58,9 +35,119 @@ impl std::str::FromStr for Operation {
     }
 }
 
+#[derive(Clone)]
 struct Instruction {
     operation: Operation,
     argument: i32,
+}
+
+#[derive(Debug)]
+enum ProgramEndReason {
+    EndOfProgram,
+    Loop,
+    InvalidProgramCounter,
+}
+
+struct ProgramEnd {
+    end_reason: ProgramEndReason,
+    accumulator: i64,
+}
+
+impl std::fmt::Debug for ProgramEnd {
+    fn fmt(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        formatter.debug_struct("ProgramEnd")
+            .field("end reason", &self.end_reason)
+            .field("accumulator", &self.accumulator)
+            .finish()
+    }
+}
+
+fn fix_and_detect_loop(input: &str) -> i64 {
+    let mut instructions = parse_program(input);
+    for index in 0..instructions.len() - 1 {
+        {
+            let mut instruction = &mut instructions[index];
+            match instruction.operation {
+                Operation::Acc => continue,
+                Operation::Jmp => instruction.operation = Operation::Nop,
+                Operation::Nop => instruction.operation = Operation::Jmp,
+            }
+        }
+
+        let result = run_program(&instructions);
+        match result.end_reason {
+            ProgramEndReason::EndOfProgram => return result.accumulator,
+            ProgramEndReason::InvalidProgramCounter => panic!("Unexpected end of program"),
+            ProgramEndReason::Loop => {},
+        }
+
+        let mut instruction = &mut instructions[index];
+        match instruction.operation {
+            Operation::Acc => panic!(format!("Unexpected operation after program end '{:?}'", instruction.operation)),
+            Operation::Jmp => instruction.operation = Operation::Nop,
+            Operation::Nop => instruction.operation = Operation::Jmp,
+        }
+    }
+
+    return -1;
+}
+
+fn detect_loop_input(input: &str) -> i64 {
+    let instructions = parse_program(input);
+    let result = run_program(&instructions);
+    
+    match result.end_reason {
+        ProgramEndReason::Loop => {
+            return result.accumulator;
+        }
+        _ => {
+            panic!(format!("Loop not detected, {:?}", result));
+        }
+    }
+}
+
+fn run_program(instructions: &Vec<Instruction>) -> ProgramEnd {
+    let mut accumulator: i64 = 0;
+    let mut program_counter: i64 = 0;
+    let instructions_count = instructions.len() as i64;
+
+    let mut instructions: Vec<(&Instruction, bool)> = instructions.iter().map(|ins| (ins, false)).collect();
+    loop {
+        if program_counter < 0 || program_counter > instructions_count {
+            return ProgramEnd {
+                end_reason: ProgramEndReason::InvalidProgramCounter,
+                accumulator: accumulator,
+            }
+        } else if program_counter == instructions_count {
+            return ProgramEnd {
+                end_reason: ProgramEndReason::EndOfProgram,
+                accumulator: accumulator,
+            }
+        }
+
+        let mut instruction = &mut instructions[program_counter as usize];
+
+        if instruction.1 {
+            return ProgramEnd {
+                end_reason: ProgramEndReason::Loop,
+                accumulator: accumulator,
+            }
+        }
+        instruction.1 = true;
+
+        match instruction.0.operation {
+            Operation::Nop => {
+                program_counter += 1;
+            },
+            Operation::Acc => {
+                accumulator += instruction.0.argument as i64;
+                program_counter += 1;
+            },
+            Operation::Jmp => {
+                program_counter += instruction.0.argument as i64;
+            }
+        }
+    }
 }
 
 fn parse_program(input: &str) -> Vec<Instruction> {
@@ -106,7 +193,7 @@ acc +6"
     #[test]
     fn example_detect_loop() {
         let input = get_example();
-        let result = detect_loop(input);
+        let result = detect_loop_input(input);
 
         assert_eq!(5, result);
     }
@@ -114,8 +201,24 @@ acc +6"
     #[test]
     fn input_detect_loop() {
         let input = get_challenge_input();
-        let result = detect_loop(input);
+        let result = detect_loop_input(input);
 
         assert_eq!(1262, result);
+    }
+
+    #[test]
+    fn example_fix_and_detect_loop() {
+        let input = get_example();
+        let result = fix_and_detect_loop(input);
+
+        assert_eq!(8, result);
+    }
+
+    #[test]
+    fn input_fix_and_detect_loop() {
+        let input = get_challenge_input();
+        let result = fix_and_detect_loop(input);
+
+        assert_eq!(1643, result);
     }
 }
