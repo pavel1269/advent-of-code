7e5cc643388d47b79e3a2d999ae72869d37e1839
pavel1269 <pavelgrim@gmail.com>
2022-12-10 09:49:14 +0100

    Day 10, part 2

--- 2022/src/day10/mod.rs
+++ 2022/src/day10/mod.rs
@@ -1,10 +1,77 @@
 use std::collections::HashMap;
 
-
 pub fn get_solution_part1() -> String {
     let input = get_input();
     let result = sum_signal_strengths(input);
     return result.to_string();
+}
+
+pub fn get_solution_part2() -> String {
+    let input = get_input();
+    draw(input);
+    //return result.to_string();
+
+    // RBPARAGP - incorrect
+
+    // ###..###..###...##..###...##...##..#####
+    // #..#.#..#.#..#.#..#.#..#.#..#.#..#.#...#
+    // #..#.###..#..#.#..#.#..#.#..#.#....###.#
+    // ###..#..#.###..####.###..####.#.##.#....
+    // #.#..#..#.#....#..#.#.#..#..#.#..#.#...#
+    // #..#.###..#....#..#.#..#.#..#..###.#....
+
+    return String::from("RBPARAGF");
+}
+
+fn draw(input: &str) -> [String; 6] {
+    let instructions = parse_input(input);
+    let mut pc = Pc::new();
+    let mut screen: Vec<char> = Vec::with_capacity(240);
+
+    let mut cycle = 1;
+    for instruction in instructions.iter() {
+        for _ in cycle..pc.cycle + 3 {
+            let cycle_int = i32::try_from(cycle).unwrap() % 40;
+            if cycle_int >= pc.x && cycle_int <= pc.x + 2 {
+                screen.push('#');
+            }
+            else {
+                screen.push('.');
+            }
+            cycle += 1;
+        }
+        instruction.run(&mut pc);
+    }
+
+    let (row1, rest) = screen.split_at(40);
+    let (row2, rest) = rest.split_at(40);
+    let (row3, rest) = rest.split_at(40);
+    let (row4, rest) = rest.split_at(40);
+    let (row5, rest) = rest.split_at(40);
+    let (row6, _) = rest.split_at(40);
+
+    let row1 = row1.iter().collect::<String>();
+    let row2 = row2.iter().collect::<String>();
+    let row3 = row3.iter().collect::<String>();
+    let row4 = row4.iter().collect::<String>();
+    let row5 = row5.iter().collect::<String>();
+    let row6 = row6.iter().collect::<String>();
+
+    println!("{}", row1);
+    println!("{}", row2);
+    println!("{}", row3);
+    println!("{}", row4);
+    println!("{}", row5);
+    println!("{}", row6);
+
+    return [
+        row1,
+        row2,
+        row3,
+        row4,
+        row5,
+        row6,
+    ];
 }
 
 fn sum_signal_strengths(input: &str) -> i32 {
