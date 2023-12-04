5c37001f8eaa8fa67ccfe5d12662b7be89e7c48e
pavel1269 <pavelgrim@gmail.com>
2023-12-04 08:42:30 +0100

    2023 day 4 part 2

--- 2023/day04/src/main.rs
+++ 2023/day04/src/main.rs
@@ -3,23 +3,49 @@ fn main() {
     let lottery_cards = parse_input(input);
     let result_part1 = calculate_winning_points(&lottery_cards);
     println!("Part1: {}", result_part1);
+
+    let result_part2 = calculate_lottery_ticket_amount(&lottery_cards);
+    println!("Part2: {}", result_part2);
+}
+
+fn calculate_lottery_ticket_amount(lottery_cards: &Vec<LotteryCard>) -> u32 {
+    let mut amount: Vec<u32> = vec![1; lottery_cards.len()];
+    for (index, card) in lottery_cards.iter().enumerate() {
+        let current_amount = amount[index];
+        let winners = get_winners_count(card);
+
+        for offset in 1..winners + 1 {
+            let new_index = index + offset as usize;
+            if new_index >= amount.len() {
+                break;
+            }
+            amount[index + offset as usize] += current_amount;
+        }
+    }
+
+    let total_amount: u32 = amount.iter().sum();
+    return total_amount;
 }
 
 fn calculate_winning_points(lottery_cards: &Vec<LotteryCard>) -> u32 {
     let mut points = 0;
     for card in lottery_cards {
-        let mut winners = 0;
-        for number in card.numbers.iter() {
-            if card.numbers_winning.contains(&number) {
-                winners += 1;
-            }
-        }
-
+        let winners = get_winners_count(card);
         if winners > 0 {
             points += u32::pow(2, winners - 1);
         }
     }
     return points;
+}
+
+fn get_winners_count(lottery_card: &LotteryCard) -> u32 {
+    let mut winners = 0;
+    for number in lottery_card.numbers.iter() {
+        if lottery_card.numbers_winning.contains(&number) {
+            winners += 1;
+        }
+    }
+    return winners;
 }
 
 #[derive(Debug)]
@@ -36,7 +62,7 @@ fn parse_input(input: &str) -> Vec<LotteryCard> {
 
         let numbers_winning_string = &captures[2];
         let numbers_string = &captures[3];
-        
+
         let numbers_winning = parse_numbers_string(numbers_winning_string);
         let numbers = parse_numbers_string(numbers_string);
 
@@ -69,5 +95,14 @@ mod tests {
         let result = calculate_winning_points(&lottery_cards);
 
         assert_eq!(result, 13);
+    }
+
+    #[test]
+    fn part2_example() {
+        let input = get_example_input();
+        let lottery_cards = parse_input(input);
+        let result = calculate_lottery_ticket_amount(&lottery_cards);
+
+        assert_eq!(result, 30);
     }
 }
