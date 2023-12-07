452d2dfb3141f8207d9a464e169fd4600978d16f
pavel1269 <pavelgrim@gmail.com>
2023-12-07 17:34:29 +0100

    2023 day 7 part 2

--- 2023/day07/src/main.rs
+++ 2023/day07/src/main.rs
@@ -2,10 +2,13 @@ use std::{collections::HashMap, cmp::Ordering};
 
 fn main() {
     let input = get_input();
-    let plays = parse_input(input);
-    
+    let plays = parse_input(input, false);
     let result_part1 = sum_winnings(&plays);
     println!("Part1: {}", result_part1);
+    
+    let plays_jokers = parse_input(input, true);
+    let result_part2 = sum_winnings(&plays_jokers);
+    println!("Part2: {}", result_part2);
 }
 
 fn sum_winnings(plays: &Vec<Play>) -> u64 {
@@ -70,38 +73,44 @@ impl Hand {
         for card in cards {
             occurences.entry(*card).and_modify(|val| *val += 1).or_insert(1 as u8);
         }
+        let jokers = *occurences.entry(1).or_default();
+        if jokers > 0 {
+            occurences.remove(&1);
+        }
         let mut duplicates = HashMap::new();
         for occurence in occurences.values() {
             duplicates.entry(*occurence).and_modify(|val| *val += 1).or_insert(1 as u8);
         }
         
-        if duplicates.contains_key(&5) {
-            return 10;
+        if jokers >= 4 || duplicates.contains_key(&(5 - jokers)) {
+            return 10; // five of a kind
         }
-        else if duplicates.contains_key(&4) {
-            return 9;
+        else if jokers == 3 || duplicates.contains_key(&(4 - jokers)) {
+            return 9; // four of a kind
         }
-        else if duplicates.contains_key(&3) && duplicates.contains_key(&2) {
-            return 8;
+        else if (duplicates.contains_key(&3) && duplicates.contains_key(&2))
+            || (duplicates.contains_key(&2) && duplicates[&2] == 2 && jokers == 1)
+            || (duplicates.contains_key(&3) && jokers == 1) {
+            return 8; // full house
         }
-        else if duplicates.contains_key(&3) {
-            return 7;
+        else if jokers == 2 || duplicates.contains_key(&(3 - jokers)) {
+            return 7; // three of a kind
         }
         else if duplicates.contains_key(&2) && duplicates[&2] == 2 {
-            return 6;
+            return 6; // two pairs
         }
-        else if duplicates.contains_key(&2) {
-            return 5;
+        else if jokers == 1 || duplicates.contains_key(&(2 - jokers)) {
+            return 5; // one pair
         }
         return 4;
     }
 }
 
-fn parse_input(input: &str) -> Vec<Play> {
+fn parse_input(input: &str, jokers: bool) -> Vec<Play> {
     let mut plays = Vec::new();
     for line in input.lines() {
         let line_parts: Vec<&str> = line.split(' ').collect();
-        let hand = parse_hand(line_parts[0]);
+        let hand = parse_hand(line_parts[0], jokers);
         let bid: u64 = line_parts[1].parse().unwrap();
         let play = Play {
             hand,
@@ -112,14 +121,18 @@ fn parse_input(input: &str) -> Vec<Play> {
     return plays;
 }
 
-fn parse_hand(input: &str) -> Hand {
+fn parse_hand(input: &str, jokers: bool) -> Hand {
     let mut cards = [0; 5];
     for (index, char) in input.chars().enumerate() {
         match char {
             'A' => cards[index] = 14,
             'K' => cards[index] = 13,
             'Q' => cards[index] = 12,
-            'J' => cards[index] = 11,
+            'J' => cards[index] = if jokers {
+                1
+            } else {
+                11
+            },
             'T' => cards[index] = 10,
             '2'..='9' => cards[index] = char as u32 - '0' as u32,
             _ => panic!(),
@@ -147,7 +160,7 @@ mod tests {
     #[test]
     fn part1_example() {
         let input = get_example_input();
-        let plays = parse_input(input);
+        let plays = parse_input(input, false);
         let result = sum_winnings(&plays);
         assert_eq!(result, 6440);
     }
@@ -155,8 +168,24 @@ mod tests {
     #[test]
     fn part1_example2() {
         let input = get_example2_input();
-        let plays = parse_input(input);
+        let plays = parse_input(input, false);
         let result = sum_winnings(&plays);
         assert_eq!(result, 6592);
+    }
+
+    #[test]
+    fn part2_example() {
+        let input = get_example_input();
+        let plays = parse_input(input, true);
+        let result = sum_winnings(&plays);
+        assert_eq!(result, 5905);
+    }
+
+    #[test]
+    fn part2_example2() {
+        let input = get_example2_input();
+        let plays = parse_input(input, true);
+        let result = sum_winnings(&plays);
+        assert_eq!(result, 6839);
     }
 }
